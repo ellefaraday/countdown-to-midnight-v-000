3c7bb8e66d9bacc445ae137b3db6e1710e54584e
Elle Faraday <faraday.elle@gmail.com>
2017-10-28 02:09:36 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -8,4 +8,11 @@ def countdown(number)
   "HAPPY NEW YEAR!"
 end
 
-countdown(3)
+def countdown_with_sleep(number)
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    number -= 1
+    sleep(1)
+  end
+  "HAPPY NEW YEAR!"
+end
